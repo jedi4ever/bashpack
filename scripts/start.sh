d711281df90dd67c7039f8502d80017515980958
Patrick Debois <Patrick.Debois@jedi.be>
2013-07-17 10:32:16 +0200

    add our libs to the LD_LIBRARY_PATH and DYLD_LIBRARY_PATH

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,6 +14,9 @@ shift
 # Now we go back to the '.run' directory
 cd $cwd
 
+export LD_LIBRARY_PATH=$tempdir/.bashpack/lib:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$tempdir/.bashpack/lib:$DYLD_LIBRARY_PATH
+
 # And launch our own node with the javascript in the tempdir 
 # And pass all the arguments left
 PATH=$tempdir/.bashpack/bin:$PATH
